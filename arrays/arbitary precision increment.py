ebc7bfa25e7190b0eb07bc14a9ef3c8617960684
vishnugopireddy <vishnu.var.reddy@gmail.com>
2019-08-29 20:16:07 +0530

    push

---
+++ arrays/arbitary precision increment.py
@@ -0,0 +1,13 @@
+'''
+Given:
+ An array of non-negative digits that represent a decimal integer.
+
+Problem:
+ Add one to the integer. Assume the solution still works even if implemented in a language with finite-precision arithmetic.
+
+Example:
+    [1, 4, 9] + 1 = [1,5,0]
+'''
+
+num = "149"
+num = [int(i) for i in num]
---
+++ arrays/two sum problem.py
@@ -0,0 +1,27 @@
+# Given a sorted array find two indices of the elements that add up to the given number
+# Under a constraint that no two elemets are repeating
+
+#APPROACH 1: O(n^2) BruteForce - Try with all possible combinations
+
+#APPROACH 2: O(n)
+
+def two_sum(arr,len,s):
+    start = 0
+    end = len - 1
+
+    while arr[start] + arr[end] != s and start < end:
+        if arr[start] + arr[end] < s:
+            start = start + 1
+        else:
+            end = end - 1
+
+    if arr[start] + arr[end] == s:
+        return start,end, arr[start] + arr[end]
+    else:
+        return None
+
+arr = [-2,1,2,4,7,11]
+len = 6
+s = 16
+
+print(two_sum(arr,len,s))
