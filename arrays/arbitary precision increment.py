4f93cc5e12e2a10f846d39730bb18aad23ac3989
vishnugopireddy <vishnu.var.reddy@gmail.com>
2019-09-01 06:40:51 +0530

    push

--- arrays/arbitary precision increment.py
+++ arrays/arbitary precision increment.py
@@ -1,6 +1,6 @@
 '''
 Given:
- An array of non-negative digits that represent a decimal integer.
+ An array(list) of non-negative digits that represent a decimal integer.
 
 Problem:
  Add one to the integer. Assume the solution still works even if implemented in a language with finite-precision arithmetic.
