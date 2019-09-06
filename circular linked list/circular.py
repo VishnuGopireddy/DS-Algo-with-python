2ce8098ad1c5136114dbdbc2e7290a56ac0fb376
VishnuGopireddy <vishnu.var.reddy@gmail.com>
2019-09-06 16:45:39 +0530

    push

--- circular linked list/circular.py
+++ circular linked list/circular.py
@@ -11,12 +11,14 @@ class circular:
         new_node = Node(data)
         if self.head == None:
             self.head = new_node
+            new_node.next = self.head
         else:
             curr = self.head
             while curr.next != self.head:
                 curr = curr.next
             curr.next = new_node
-        new_node.next = self.head
+            new_node.next = self.head
+
 
     def print_list(self):
         curr = self.head
@@ -24,30 +26,33 @@ class circular:
             print(curr.data)
             curr = curr.next
         print(curr.data)
-
+        print('-'*100)
     def delete_item(self,item):
         if self.head == None:
             return False
+
         else:
             curr = self.head
             prev = curr
             while curr.next != self.head:
                 if curr.data == item:
+                    print("got")
                     prev.next = curr.next
-                    prev = curr
-                    curr = curr.next
-                else:
-                    prev = curr
-                    curr = curr.next
+                prev = curr
+                curr = curr.next
         return True
 
 c = circular()
 c.append(10)
 c.append(15)
-c.append(20)
-c.append(35)
-c.append(40)
+c.append(25)
+c.print_list()
+c.delete_item(10)
+c.print_list()
 c.append(50)
-c.delete_item(35)
 c.print_list()
 
+#print(c.head.data)
+#print(c.head.next.data)
+#print(c.head.next.next.data)
+#print(c.head.next.next.next.data)
