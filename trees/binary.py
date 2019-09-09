b9090c4dadfcd884e42b91ba2ac448a0456cd07c
VishnuGopireddy <vishnu.var.reddy@gmail.com>
2019-09-09 16:01:51 +0530

    push

---
+++ trees/binary.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class btree:
+    def __init__(self):
+        self.head = None
+
+    def insert(self,data):
+        if self.head == None:
+            self.head = Node(data)
+        else:
+
