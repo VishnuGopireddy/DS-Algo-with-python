3755c929ece653ce90961b448cac2733bb8e774e
VishnuGopireddy <vishnu.var.reddy@gmail.com>
2019-09-11 16:05:18 +0530

    push

--- trees/binary.py
+++ trees/binary.py
@@ -40,6 +40,12 @@ class BinaryTree:
                 q.append(node.left)
             if node.right:
                 q.append(node.right)
+    def height(self,start):
+        h = 0
+        if start == None:
+            return h
+        else:
+            return max(self.height(start.left) + 1,self.height(start.right) + 1)
 
 '''
                 10
@@ -47,6 +53,8 @@ class BinaryTree:
             15      8
            / \        \            
          20   30       50 
+                         \
+                          150
 '''
 from queue import Queue
 b = BinaryTree(10)
@@ -55,6 +63,7 @@ b.root.right = Node(8)
 b.root.left.left = Node(20)
 b.root.left.right = Node(30)
 b.root.right.right = Node(50)
+b.root.right.right.right = Node(150)
 
 print("Pre-order is :",end=' ')
 b.preorder(b.root)
@@ -67,3 +76,6 @@ b.postorder(b.root)
 
 print("Level order is :",end=' ')
 b.levelorder(b.root)
+
+print("Height of tree is :",end=' ')
+print("height is",b.height(b.root))
