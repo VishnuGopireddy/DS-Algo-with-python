2a3fc334d8095621ce03db0fc028f6601205c8fb
VishnuGopireddy <vishnu.var.reddy@gmail.com>
2019-10-01 12:01:45 +0530

    push

--- Algorithms/binary_search.py
+++ Algorithms/binary_search.py
@@ -1,13 +1,11 @@
 #BINARY SEARCH
-
 my_list = [1,4,8,9,12,16,19,25,29,35]
 
-ele = 4
-n = len(my_list) - 1
-
+ele = 1
+n = len(my_list)
 def binarysearch(my_list,ele,low=0,high=n):
     if high >= low:
-        mid = (low + high-1) // 2
+        mid = low + (high-1) // 2
         if my_list[mid] == ele:
             return mid
         elif ele < my_list[mid]:
@@ -17,7 +15,7 @@ def binarysearch(my_list,ele,low=0,high=n):
     else:
         return False
 
-mid = binarysearch(my_list,ele,0,n)
+mid = binarysearch(my_list,ele,0,n-1)
 if mid:
     print(ele,'is found at position',mid)
 else:
