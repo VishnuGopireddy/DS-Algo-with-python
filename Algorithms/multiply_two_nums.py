106decdeab70ffa5cb1a344dae78a119b1cc3def
VishnuGopireddy <vishnu.var.reddy@gmail.com>
2019-10-03 15:48:51 +0530

    push

---
+++ Algorithms/multiply_two_nums.py
@@ -0,0 +1,18 @@
+'''
+x = 5
+y = 3
+(5,3)
+5 + (5,2)
+5 + (5,1)
+5 + (5,0)
+'''
+
+x = 5
+y = 3
+def multiply(x,y):
+    if y == 0:
+        return 0
+    else:
+        return 5 + multiply(5,y-1)
+
+print(multiply(x,y))
