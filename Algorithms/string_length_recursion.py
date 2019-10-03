b5fd484a2aaaed308b8901a0635c9d48b22ef5b5
VishnuGopireddy <vishnu.var.reddy@gmail.com>
2019-10-03 13:36:16 +0530

    push

---
+++ Algorithms/string_length_recursion.py
@@ -0,0 +1,19 @@
+#calculate lenth of the string
+st = 'Welcome to the world'
+#predefined function
+print(len(st))
+#Iterative
+count = 0
+for i in st:
+    count +=1
+print(count)
+
+#recursive
+
+def word_count(st):
+    if st:
+        return 1 + word_count(st[1:])
+    else:
+        return 0
+
+print(word_count(st))
