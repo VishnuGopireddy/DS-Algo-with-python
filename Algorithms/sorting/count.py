bbea3b4ab3c617371b0c8cde4eb7b8b25c76f0e5
vishnu <vishnu.var.reddy@gmail.com>
2019-10-07 01:26:28 +0530

    push

--- Algorithms/sorting/count.py
+++ Algorithms/sorting/count.py
@@ -1,3 +1,16 @@
 '''
 count sort:
+
+Example: consider array in range (0,9)
 '''
+
+arr = [1, 4, 1, 2, 7, 5, 2]
+low = 0
+high = 9
+count = [0 for i in range(low,high+1)]
+
+for i in arr:
+    count[i] = count[i] + 1
+print(count)
+
+print(sort)
