2d86d93c005ac6fd2726eccc0ca60c6210a99101
vishnu <vishnu.var.reddy@gmail.com>
2019-10-06 06:53:10 +0530

    push

--- Algorithms/sorting/selection.py
+++ Algorithms/sorting/selection.py
@@ -1,3 +1,18 @@
 '''
 selection sort:
 '''
+
+arr = [5, 6, 1, 7, 0, 4, 12, 10, 9]
+n = len(arr)
+
+def selection(arr,n):
+    for i in range(0,n):
+        min = i
+        for j in range(i,n):
+            if arr[j] < arr[min]:
+                min = j
+        arr[i],arr[min] = arr[min],arr[i]
+
+
+selection(arr,n)
+print(arr)
