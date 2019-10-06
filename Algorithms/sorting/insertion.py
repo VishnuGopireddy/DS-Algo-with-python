4226619013e1794a99cb5be34147465b04a773a5
vishnu <vishnu.var.reddy@gmail.com>
2019-10-06 06:24:19 +0530

    push

--- Algorithms/sorting/insertion.py
+++ Algorithms/sorting/insertion.py
@@ -5,9 +5,13 @@ arr = [170, 45, 75, 90, 802, 24, 2, 66]
 
 n = len(arr)
 
-for i in range(1, n):
-    j = i - 1
+def insertion(arr,n):
+    for i in range(1,n):
+        j = i - 1
+        while arr[i] < arr[j] and j >= 0:
+            arr[j],arr[j+1] = arr[j+1],arr[j]
+            j = j - 1
+        arr[i],arr[j] = arr[j],arr[i]
 
-
-
-
+insertion(arr)
+print(arr)
