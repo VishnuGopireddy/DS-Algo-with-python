d8f60213aa625798c60cb9c594fce56ba21d269c
vishnu <vishnu.var.reddy@gmail.com>
2019-10-07 10:43:44 +0530

    push

--- Algorithms/sorting/count.py
+++ Algorithms/sorting/count.py
@@ -9,19 +9,25 @@ SPCAE : O(n+k)
 Example: consider array in range (0,9)
 '''
 
+
+def counting(arr,low,high):
+    sort = []
+    count = [0 for i in range(low,high+1)]
+    for i in arr:
+        count[i] = count[i] + 1
+    print(count)
+
+    j = 0
+    for i in range(len(count)):
+        while count[i] > 0:
+            sort.append(i)
+            count[i] -= 1
+    return sort
+
+
 arr = [1, 4, 1, 2, 7, 5, 2]
 low = 0
 high = 9
-count = [0 for i in range(low,high+1)]
-sort = []
 
-for i in arr:
-    count[i] = count[i] + 1
-print(count)
-
-j = 0
-for i in range(len(count)):
-    while count[i] > 0:
-        sort.append(i)
-        count[i] -= 1
+sort = counting(arr, low, high)
 print(sort)
--- Algorithms/sorting/merge.py
+++ Algorithms/sorting/merge.py
@@ -1,3 +1,10 @@
 arr = [5, 6, 1, 7, 0, 4, 12, 10, 9]
 n = len(arr)
 
+arr1 = [3, 4, 5, 9, 12]
+arr2 = [1, 2, 6, 11]
+def merge(arr1, arr2):
+    new_arr = []
+    i = 0
+    j = 0
+    l1, l2 = len(arr1), len(arr2)
