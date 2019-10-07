b7ad62ab3efca9a5817648b1f88973da19d4078d
vishnu <vishnu.var.reddy@gmail.com>
2019-10-07 10:24:27 +0530

    push

--- Algorithms/sorting/count.py
+++ Algorithms/sorting/count.py
@@ -1,5 +1,10 @@
 '''
-count sort:
+count sort: Count the coccurances in a list, and convert occurances list to form a original list.
+
+BEST CASE: O(n+k)
+AVG CASE: O(n+k)
+WORST CASE: O(n+k)
+SPCAE : O(n+k)
 
 Example: consider array in range (0,9)
 '''
@@ -8,9 +13,15 @@ arr = [1, 4, 1, 2, 7, 5, 2]
 low = 0
 high = 9
 count = [0 for i in range(low,high+1)]
+sort = []
 
 for i in arr:
     count[i] = count[i] + 1
 print(count)
 
+j = 0
+for i in range(len(count)):
+    while count[i] > 0:
+        sort.append(i)
+        count[i] -= 1
 print(sort)
