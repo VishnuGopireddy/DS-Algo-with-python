fdf62236ad12ddd3554646c132622fbbd621956f
vishnu <vishnu.var.reddy@gmail.com>
2019-10-08 11:57:56 +0530

    push

--- Algorithms/greedy/jobseq_with_deadlines.py
+++ Algorithms/greedy/jobseq_with_deadlines.py
@@ -3,3 +3,14 @@ Given an array of jobs where every job has a deadline and associated profit if t
 It is also given that every job takes single unit of time, so the minimum possible deadline for any job is 1.
 How to maximize total profit if only one job can be scheduled at a time.
 '''
+
+def jobseq(arr):
+    pass
+
+arr = [['a', 2, 100], # Job Array
+       ['b', 1, 19],
+       ['c', 2, 27],
+       ['d', 1, 25],
+       ['e', 3, 15]]
+
+
