882d844bc290138b0304be4073b978fee740444b
vishnu <vishnu.var.reddy@gmail.com>
2019-10-09 07:35:36 +0530

    push

--- Algorithms/greedy/jobseq_with_deadlines.py
+++ Algorithms/greedy/jobseq_with_deadlines.py
@@ -1,7 +1,10 @@
 '''
+https://www.geeksforgeeks.org/job-sequencing-problem/
 Given an array of jobs where every job has a deadline and associated profit if the job is finished before the deadline.
 It is also given that every job takes single unit of time, so the minimum possible deadline for any job is 1.
 How to maximize total profit if only one job can be scheduled at a time.
+
+TIME Complexity => O(nlogn) + O (n)
 '''
 
 def jobseq(arr):
