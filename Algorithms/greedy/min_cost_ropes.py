eb233928881565461dc57adf9b1351fff8807248
vishnu <vishnu.var.reddy@gmail.com>
2019-10-11 12:36:23 +0530

    push

---
+++ Algorithms/greedy/min_cost_ropes.py
@@ -0,0 +1,20 @@
+#https://practice.geeksforgeeks.org/problems/minimum-cost-of-ropes/0/?ref=self
+'''
+There are given N ropes of different lengths, we need to connect these ropes into one rope.
+The cost to connect two ropes is equal to sum of their lengths. The task is to connect the ropes with minimum cost.
+'''
+def min_cost_ropes(ropes,n):
+    cost = 0
+    while n > 1:
+        r1 = min(ropes)
+        ropes.remove(r1)
+        r2 = min(ropes)
+        ropes.remove(r2)
+        ropes.append(r1 + r2)
+        cost = cost + r1 + r2
+        n = n -1
+    return cost
+
+for kase in range(kases):
+    ropes = list(map(int,input().split))
+print(min_cost_ropes(ropes,n))
