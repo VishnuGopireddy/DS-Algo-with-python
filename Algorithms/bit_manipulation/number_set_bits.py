84b8abf8beb123f6f60427ead07ff876d336ff48
akhileshkumargangwar <gangwarakhi1993@gmail.com>
2019-10-12 13:58:30 +0530

    push

--- Algorithms/bit_manipulation/number_set_bits.py
+++ Algorithms/bit_manipulation/number_set_bits.py
@@ -2,13 +2,13 @@
 '''
 https://www.geeksforgeeks.org/count-set-bits-in-an-integer/
 '''
-def count_set_bits(n)
+def count_set_bits(n):
     '''
     :param n: integer 
     :return: count as integer
     '''
     return bin(n).count('1')
 
-n = input('enter a number')
+n = int(input('enter a number'))
 count = count_set_bits(n)
 print(count)
