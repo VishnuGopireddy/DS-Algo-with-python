2c73117714c5b3f02a60d8acde5ea4477f2ac579
akhileshkumargangwar <gangwarakhi1993@gmail.com>
2019-10-12 13:44:44 +0530

    push

--- Algorithms/bit_manipulation/XOR_from_1-n.py
+++ Algorithms/bit_manipulation/XOR_from_1-n.py
@@ -10,3 +10,20 @@ case 2: If n % 4 == 1 then return 1
 case 3: If n % 4 == 2 then return n + 1
 case 4: If n % 4 == 3 then return 0
 '''
+def XOR_1_to_n(n):
+    '''
+    Calculates XOR from 1 to n
+    :param n: int
+    :return: value of xor from 1 to n
+    '''
+    if n % 4 == 0:
+        return n
+    elif n % 4 == 1:
+        return 1
+    elif n % 4 == 2:
+        return n + 1
+    elif n % 4 == 3:
+        return 0
+n = int(input('Enter n value'))
+result = XOR_1_to_n(n)
+print(result)
