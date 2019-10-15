9ead8878093d9f82ba6f3451aa68413175746913
vishnu <vishnu.var.reddy@gmail.com>
2019-10-15 10:33:43 +0530

    push

--- Algorithms/mathematics/GCD.py
+++ Algorithms/mathematics/GCD.py
@@ -1,5 +1,7 @@
 #https://www.geeksforgeeks.org/c-program-find-gcd-hcf-two-numbers/
 '''
+GCD (Greatest Common Divisor) or HCF (Highest Common Factor) of two numbers is the largest number that divides both of them.
+
 An efficient solution is to use Euclidean algorithm which is the main algorithm used for this purpose.
 The idea is, GCD of two numbers doesn’t change if smaller number is subtracted from a bigger number.
 '''
@@ -15,3 +17,8 @@ def gcd(a,b):
             return gcd(a-b,b)
         else:
             return gcd(a,b-a)
+
+a = 15
+b = 45
+
+print('GCD of %d and %d id %d' % (a,b,gcd(a,b)))
