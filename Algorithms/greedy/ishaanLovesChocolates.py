00174b7963bc4ffa942b5cd42de80c3389430c84
VishnuGopireddy <vishnu.var.reddy@gmail.com>
2019-10-24 16:21:44 +0530

    push

--- Algorithms/greedy/ishaanLovesChocolates.py
+++ Algorithms/greedy/ishaanLovesChocolates.py
@@ -1,6 +1,7 @@
 #code
 #https://practice.geeksforgeeks.org/problems/ishaan-loves-chocolates/0
 kases = int(input())
+
 for kase in range(kases):
     n = int(input())
     tasty = list(map(int,input().split(' ')))
