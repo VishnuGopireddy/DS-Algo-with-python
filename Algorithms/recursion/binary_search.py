3965dab28be2af5265d5d99414748fc6ea005c4c
vishnu <vishnu.var.reddy@gmail.com>
2019-10-26 21:36:47 +0530

    push

--- Algorithms/recursion/binary_search.py
+++ Algorithms/recursion/binary_search.py
@@ -7,6 +7,7 @@ def binarysearch(my_list,ele,low=0,high=n):
     if high >= low:
         mid = low + (high-1) // 2
         if my_list[mid] == ele:
+
             return mid
         elif ele < my_list[mid]:
             return binarysearch(my_list,ele,low=0,high=mid-1)
