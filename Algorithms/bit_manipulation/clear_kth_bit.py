5ac2152fc4c1f428eae86773060be64ed028016b
vishnu <vishnu.var.reddy@gmail.com>
2019-10-27 21:05:01 +0530

    push

---
+++ Algorithms/bit_manipulation/clear_kth_bit.py
@@ -0,0 +1,23 @@
+#RBR interview prep
+#set kth bit of a number
+'''
+we assume that index of LSB is 0 here k = 3
+ex: 1001010
+and 1110111 (to get this: complement of (1 << k))
+-------------
+ans 1000010  ----> return
+-------------
+TIME: O(log n)
+'''
+
+
+def clear_kth_bit(num, k):
+    '''
+    :param num: integer, number to test
+    :param k: integer, position to test
+    :return: integer, with kth bit clear
+    '''
+    return (num & ~(1 << (k - 1)))
+
+
+print(clear_kth_bit(32,2))
