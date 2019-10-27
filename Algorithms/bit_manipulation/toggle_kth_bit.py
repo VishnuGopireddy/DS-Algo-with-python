b22df1a63ecacd283cf3ab325abcbfea92ec563b
vishnu <vishnu.var.reddy@gmail.com>
2019-10-27 21:08:30 +0530

    push

--- Algorithms/bit_manipulation/toggle_kth_bit.py
+++ Algorithms/bit_manipulation/toggle_kth_bit.py
@@ -1,9 +1,9 @@
 #RBR interview prep
-#set kth bit of a number
+#toggle kth bit of a number
 '''
 we assume that index of LSB is 0 here k = 3
 ex: 1000010
-or  0001000 (to get this: 1 << k)
+xor 0001000 (to get this: 1 << k)
 -------------
 ans 1001010  ----> return
 -------------
@@ -11,14 +11,14 @@ TIME: O(log n)
 '''
 
 
-def set_kth_bit(num, k):
+def toggle_kth_bit(num, k):
     '''
     :param num: integer, number to test
     :param k: integer, position to test
     :return: integer, with kth bit set
     '''
     num2 = 1 << k
-    return num | num2
+    return num ^ num2
 
 
-print(set_kth_bit(89, 2))
+print(toggle_kth_bit(89, 2))
