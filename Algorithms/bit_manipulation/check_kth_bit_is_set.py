d72370f875a4f00c5f00da0e430af5980914d0b4
vishnu <vishnu.var.reddy@gmail.com>
2019-10-27 19:28:33 +0530

    push

--- Algorithms/bit_manipulation/check_kth_bit_is_set.py
+++ Algorithms/bit_manipulation/check_kth_bit_is_set.py
@@ -1,5 +1,5 @@
-#RBR Interview Prep
-#Check kth bit is set or not
+# RBR Interview Prep
+# Check kth bit is set or not
 '''
 we assume that index of LSB is 0 here k = 3
 ex: 1001010
@@ -7,9 +7,11 @@ and 0001000 (to get this: 1 << k)
 -------------
 ans 0001000  ----> if set then sol > 0; else sol = 0
 -------------
+TIME: O(logn)
 '''
 
-def check_kth_bit(num,k):
+
+def check_kth_bit(num, k):
     '''
     :param num: integer, number to test
     :param k: integer, position to test
@@ -22,4 +24,5 @@ def check_kth_bit(num,k):
     else:
         return 0
 
-print(check_kth_bit(89,2))
+
+print(check_kth_bit(89, 2))
