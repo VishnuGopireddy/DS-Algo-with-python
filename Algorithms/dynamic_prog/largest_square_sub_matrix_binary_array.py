906294694370ceeed595b2ac934d8f7b33ea0a4a
vishnu <vishnu.var.reddy@gmail.com>
2019-10-29 01:47:11 +0530

    push

--- Algorithms/dynamic_prog/largest_square_sub_matrix_binary_array.py
+++ Algorithms/dynamic_prog/largest_square_sub_matrix_binary_array.py
@@ -13,6 +13,18 @@ Approach 1 : (Brute Force) => O(N^4)
 Approach 2 : Dynamic Programing +> O(n ^ 2)
 
 '''
+def display_mat(mat,size):
+    '''
+    Display the given matrix
+    :param mat: multi dimensional array matrix
+    :param size: tuple with size
+    :return: None
+    '''
+    # display mat
+    for i in range(size[0]):
+        for j in range(size[1]):
+            print(mat[i][j], end=' ')
+        print(' ')
 
 
 def largest_sub_matrix(mat, size):
@@ -23,13 +35,23 @@ def largest_sub_matrix(mat, size):
     :return: size of largest square sub-matrix in a given binary matrix
     '''
     sol = [[0 for i in range(size[1])] for j in range(size[0])]
+    # display mat
+    display_mat(mat, size)
+    print('----------------')
     for i in range(size[0]):
         for j in range(size[1]):
-            print(sol[i][j], end=' ')
-        print(' ')
+            if i == 0 or j == 0:
+                sol[i][j] = mat[i][j]
+    for i in range(1, size[0]):
+        for j in range(1, size[1]):
+            if mat[i][j] == 1:
+                sol[i][j] = min(sol[i-1][j-1], sol[i][j-1], sol[i-1][j]) + 1
+            else:
+                sol[i][j] = 0
+
+    display_mat(sol,size)
 
 size = (5,4)
 mat = [[1, 1, 1, 1], [0, 0, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
 
-print (mat)
 largest_sub_matrix(mat, size)
