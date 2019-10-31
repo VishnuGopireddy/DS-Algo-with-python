9847881faf5f00ea64ca4d3fa23945684f09d847
vishnu <vishnu.var.reddy@gmail.com>
2019-11-01 00:57:16 +0530

    push

---
+++ Algorithms/dynamic_prog/longest_increasing_subsequence.py
@@ -0,0 +1,13 @@
+#RBR Interv prep
+#Find the longest increasing subsequence
+'''
+Example: [2,10,13,15,3,11,12]
+longestincreasing subsequences are: [2,3,11,12], [2,10,13,15], [10,13,15]
+Approach1:
+    arr = [2,10,13,15,3,11,12]
+    arr1 = [2,3,10,11,12,13,15] ==> sorted array
+    Perform Longest common subsequence on arr, arr1
+    TIME and Space = O(n^2)
+Approach 2:
+
+'''
