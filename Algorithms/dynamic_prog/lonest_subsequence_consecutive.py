28bb7b5964b342bfdf8110da05622f754b269f51
vishnu <vishnu.var.reddy@gmail.com>
2019-11-01 00:47:23 +0530

    push

---
+++ Algorithms/dynamic_prog/lonest_subsequence_consecutive.py
@@ -0,0 +1,20 @@
+#RBR Interview Prep-
+#Find the longest subsequence in an array, such that elements in subsequence are consecutive
+'''
+Find the longest subsequence in an array, such that elements in subsequence are consecutive
+Example: [10,4,3,11,13,5,6,12,7]
+possible subsequence are: 1. [4,3,5,6,7] and [10,11,13,12]
+return longest subsequence
+
+Approach 1: (Sort)
+1. Sort out the array
+2. Find the length of the longest sub-sequence
+Time: O(nlogn) + O(n)
+
+Approach2: (Hash Table)
+1. Crate hash table with all the elements in the array and key is true.
+2. For a given number in the hash table Check for the series and return longest sequence.
+TIme: O(n) + O(2n) + On()
+Space:  O(n)
+
+'''
