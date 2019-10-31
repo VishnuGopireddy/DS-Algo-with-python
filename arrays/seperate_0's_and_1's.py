3c6b9048bca298bed200abc236f9cb2b12e4046e
vishnu <vishnu.var.reddy@gmail.com>
2019-10-31 05:48:50 +0530

    push

---
+++ arrays/seperate_0's_and_1's.py
@@ -0,0 +1,16 @@
+#RBR Interview PREP
+#Seperate 0's and 1's in an array
+'''
+Given a binary array(only 0's and 1's seperate all zero's and ones's in an array
+Example: 1001110
+Sol: 0001111
+
+Approach 1: (Count sort)
+count all 1's and 0's then add all zeros in the front and 1's at the end.
+TIME: O(n)
+
+Approach 2:
+Take left pointer and right pointer do swapping
+TIME: O(N)
+
+'''
