7f2e25ccdfa7874f38e0c0954ec3ac7ac6823a62
vishnu <vishnu.var.reddy@gmail.com>
2019-11-01 07:53:18 +0530

    push

--- Algorithms/dynamic_prog/longest_increasing_subsequence.py
+++ Algorithms/dynamic_prog/longest_increasing_subsequence.py
@@ -8,6 +8,35 @@ Approach1:
     arr1 = [2,3,10,11,12,13,15] ==> sorted array
     Perform Longest common subsequence on arr, arr1
     TIME and Space = O(n^2)
-Approach 2:
-
+Approach 2: Bruteforce
+    Generate all possible subarrays (2^n).
+    Check weather the elements in the subarray are in increasing order or not.
+    TIME: O(2^n * n)
+Approach 3: Dynamic Programing
+    TIME: O(n^2)
+    SPACE : O(n)
 '''
+
+
+def longest_increasing_subseq(arr):
+    '''
+    Returns longest increasing sub-sequence from a given array
+    :param str: string
+    :return: Integer, wit longest increasing sub-sequence
+    '''
+    n = len(arr)
+    sol = [0 for i in range(n)]
+    for i in range(n):
+        if i == 0:
+            sol[0] = 1
+        else:
+            for j in range(0,i):
+                max_sub = 0
+                if arr[j] > arr[i]:
+                    if  sol[i] + 1 > max_sub:
+                        sol[j] = sol[i] + 1
+    return max(sol)
+
+arr = [2,10,13,15,3,11,12]
+print(longest_increasing_subseq(arr))
+
