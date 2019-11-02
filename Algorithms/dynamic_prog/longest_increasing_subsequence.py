ad9e098e2a749476b6cda8dac14eb3cb84f563b6
vishnu <vishnu.var.reddy@gmail.com>
2019-11-02 21:21:57 +0530

    push

--- Algorithms/dynamic_prog/longest_increasing_subsequence.py
+++ Algorithms/dynamic_prog/longest_increasing_subsequence.py
@@ -39,6 +39,7 @@ def longest_increasing_subseq(arr):
                         sol[i] = temp
     return max(sol)
 
+
 #arr = [2,10,13,15,3,11,12]
 arr = [2,3,1,5,12,10,11]
 print(longest_increasing_subseq(arr))
