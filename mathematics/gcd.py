c3743497f72fd9cf1fb4ef35e12b63e0e5900cf2
Vishnu Gopireddy <vishnu.var.reddy@gmail.com>
2019-11-06 18:38:04 +0530

    Update gcd.py

--- mathematics/gcd.py
+++ mathematics/gcd.py
@@ -5,6 +5,7 @@ GCD of two numbers is the largest number that divides both of them.
 An efficient solution is to use Euclidean algorithm which is the main algorithm used for this purpose.
 The idea is, GCD of two numbers doesn’t change if smaller number is subtracted from a bigger number.
 '''
+
 def gcd(a,b):
     if a == 0:
         return b
