c1a777e50afb85597c93ec22223c1d13b967f9a5
coea <vishnu.var.reddy@gmail.com>
2019-11-22 00:56:17 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Algorithms/backtracking/combination_strings.py

--- strings/removeDuplicates.py
+++ strings/removeDuplicates.py
@@ -5,17 +5,22 @@ Output: 'abcde'
 
 Approach 1:
 1. create a new array to store output.
-2. add new chars to yhe output after comparing with previously inserted values
+2. add new chars to the output after comparing with previously inserted values
 
 Time: O(n^2)
 Space: O(N)
 
 Approach 2:
 1. create a new array to store output and maintain a hash table to store occurances.
-2. add new chars to the output after comparing with previously inserted values
+2. add new chars to the output after checking with hash table
 
-Time: O(n^2)
-Space: O(N)
+Time: O(n)
+Space: O(N) + O(k)   k = #symbols
+
+Approach 3(inplace of approach 2):
+
+Time: O(n)
+Space: O(1) + O(k)
 
 
 '''
